a654ab087ced355d7aa4e7209ae9177c581484c5
xdawidr <dawid.rychlinski@gmail.com>
2021-02-06 13:28:56 +0100

    AddIdentityServices

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +18,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
