76749a509a27c82ff6e4c955df799d41f41d48c3
xdawidr <dawid.rychlinski@gmail.com>
2021-02-15 12:48:46 +0100

    Add Errors

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardDetailComponent } from './dashboard/dashboard-detail/dashboard-detail.component';
 import { DashboardListComponent } from './dashboard/dashboard-list/dashboard-list.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HomeComponent } from './home/home.component';
 import { TransactionsComponent } from './transactions/transactions.component';
 import { AuthGuard } from './_guards/auth.guard';
@@ -18,6 +21,9 @@ const routes: Routes = [
       {path: 'transactions', component: TransactionsComponent},
     ]
   },
+  {path: 'errors', component: TestErrorsComponent},
+  {path: 'not-found', component: NotFoundComponent},
+  {path: 'server-error', component: ServerErrorComponent},
   {path: '**', component: HomeComponent, pathMatch: 'full'},
 ];
 
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -24,9 +24,6 @@ export class NavComponent implements OnInit {
   login(){
     this.accountService.login(this.model).subscribe(response => {
       this.router.navigateByUrl('/dashboard');
-    }, error => {
-      console.log(error);
-      this.toastr.error(error.error);
     })
   }
 
